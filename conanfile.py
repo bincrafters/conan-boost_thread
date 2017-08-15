ac01484ac494a78e336c4bc58b775809db2ec56e
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:21 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostThreadConan(ConanFile):
     name = "Boost.Thread"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/thread"
+    url = "https://github.com/bincrafters/conan-boost-thread"
+    source_url = "https://github.com/boostorg/thread"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     requires =  "Boost.Level11Group/1.64.0@bincrafters/testing"
