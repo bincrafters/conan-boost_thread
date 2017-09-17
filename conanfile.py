662fb6c602f81d5afecb8b2ecd1ab744e0f3538f
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:30:39 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -3,14 +3,14 @@ from conans import ConanFile, tools, os
 
 class BoostThreadConan(ConanFile):
     name = "Boost.Thread"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-thread"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    requires =  "Boost.Level11Group/1.64.0@bincrafters/testing"
+    requires =  "Boost.Level11Group/1.65.1@bincrafters/testing"
         
     #This library is part of one or more cyclic dependency groups within Boost.
     
