eb518daa9161581c1629633192762b7d84850e44
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:35 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,9 +3,9 @@ from conans import ConanFile
 
 class BoostThreadConan(ConanFile):
     name = "Boost.Thread"
-    version = "1.65.1"
+    version = "1.66.0"
     requires = \
-        "Boost.Level11Group/1.65.1@bincrafters/testing"
+        "Boost.Level11Group/1.66.0@bincrafters/testing"
     lib_short_names = ["thread"]
     is_header_only = False
     is_in_cycle_group = True
@@ -13,9 +13,9 @@ class BoostThreadConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-thread"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
