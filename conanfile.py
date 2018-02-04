cd95e159210641afcbce1bac54de9083292ead5b
Jerry W <jerrywiltse@gmail.com>
2018-02-04 00:01:14 -0500

    change ver to 1.65.1 after merge

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conans import ConanFile, tools
 
 class BoostThreadConan(ConanFile):
     name = "boost_thread"
-    version = "1.66.0"
+    version = "1.65.1"
     url = "https://github.com/bincrafters/conan-boost_thread"
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
@@ -18,8 +18,8 @@ class BoostThreadConan(ConanFile):
     default_options = "shared=False", "threadapi=default"
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing",
-        "boost_level11group/1.66.0@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/testing",
+        "boost_level11group/1.65.1@bincrafters/testing"
     )
 
     def package_id_additional(self):
@@ -35,7 +35,7 @@ class BoostThreadConan(ConanFile):
     short_paths = True
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
