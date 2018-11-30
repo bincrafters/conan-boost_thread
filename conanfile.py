762440bd5d6df8d5d950516d84951dce0d6146ab
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:13 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,18 +4,19 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostThreadConan(base.BoostBaseConan):
     name = "boost_thread"
     url = "https://github.com/bincrafters/conan-boost_thread"
     lib_short_names = ["thread"]
-    cycle_group = "boost_level11group"
-    options = {"shared": [True, False], "threadapi": ['default', 'win32', 'pthread']}
+    cycle_group = "boost_cycle_group_c"
+    options = {
+        "shared": [True, False],
+        "threadapi": ['default', 'win32', 'pthread']
+    }
     default_options = "shared=False", "threadapi=default"
-    b2_requires = [
-        "boost_level11group",
-    ]
+    b2_requires = ["boost_cycle_group_c"]
 
     def package_info_additional(self):
         if self.settings.os == "Linux":
