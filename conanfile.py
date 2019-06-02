7ca25319175166ef7e403e4c409c426886ed9935
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:08 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,28 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostThreadConan(base.BoostBaseConan):
     name = "boost_thread"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_thread"
-    lib_short_names = ["thread"]
-    cycle_group = "boost_cycle_group_c"
-    options = {
-        "shared": [True, False],
-        "threadapi": ['default', 'win32', 'pthread']
-    }
-    default_options = "shared=False", "threadapi=default"
-    b2_requires = ["boost_cycle_group_c"]
-    
-    def configure_options_additional(self):
-        self.options["boost_cycle_group_c"].threadapi = self.options.threadapi
-
-    def package_info_additional(self):
-        if self.settings.os == "Linux":
-            self.cpp_info.libs.append("rt")
-
-    def package_id_additional(self):
-        self.info.header_only()
-        self.info.settings.os = str(self.settings.os)
+    version = "1.70.0"
